9ab5dbdbfc2edec4f1416ed9bf0a21c4ac4cac4e
Jhansi Lakshmi <djhansi@altimetrik.com>
2025-04-09 10:02:17 +0530

    Push configurations

--- Airline.API/Program.cs
+++ Airline.API/Program.cs
@@ -1,3 +1,11 @@
+using Airline.Application.IService;
+using Airline.Application.IService.Service;
+using Airline.Domain;
+using Airline.Infrastructure;
+using Airline.Infrastructure.IServices;
+using Airline.Infrastructure.IServices.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +15,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+// Register custom services, repositories etc.
+
+builder.Services.AddDbContext<AirlineDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBookingService, BookingService>();
+builder.Services.AddScoped<IRepository<Booking>, BookingRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +31,11 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    //    c =>
+    //{
+    //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Airline Management API v1");
+    //    c.RoutePrefix = string.Empty; // Launch at root
+    //});
 }
 
 app.UseHttpsRedirection();
